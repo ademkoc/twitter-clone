a27c1667c40af8999996445a850b8b81fc3f45a5
admkc <koc.adem.809@gmail.com>
2020-12-17 07:38:49 +0300

    project setup

---
+++ pages/index.js
@@ -0,0 +1,9 @@
+import React from 'react'
+import Button from '../components/button'
+
+export default function HomePage() {
+  return <div>
+      <h1>Welcome to Next.js</h1>
+      <Button>Merhaba</Button>
+  </div>
+}

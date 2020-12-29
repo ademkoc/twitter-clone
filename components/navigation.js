5236450cd94a40436126d078c495f672a639dc2e
admkc <koc.adem.809@gmail.com>
2020-12-29 12:56:46 +0300

    dynamic navigation selected item style

--- components/navigation.js
+++ components/navigation.js
@@ -1,4 +1,5 @@
 import React from 'react'
+import { useRouter } from 'next/router'
 
 import { MENU } from '../constants/index'
 import styles from './navigation.module.css'
@@ -6,12 +7,13 @@ import styles from './navigation.module.css'
 import NavigationButton from './navigation-button'
 import TextTitle from './text-title'
 
-function Navigation({ flat = false, selectedItemKey = 'home' }) {
+function Navigation({ flat = false }) {
+  const router = useRouter()
   return (
     <nav className={styles.nav}>
       {MENU.map((menu) => {
         const showTitle = !flat && menu.title.length > 0
-        const selected = menu.key == selectedItemKey
+        const selected = menu.path == router.pathname
         return (
           <NavigationButton
             key={menu.key}

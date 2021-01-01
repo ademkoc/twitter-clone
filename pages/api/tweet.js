a4cdfc7ebb9a3478b1ebd24f6144626c5cd8c44d
admkc <koc.adem.809@gmail.com>
2021-01-01 19:30:25 +0300

    added twit package and twiter api connection

---
+++ pages/api/tweet.js
@@ -0,0 +1,14 @@
+import Twit from '../../lib/twit'
+
+export default (req, res) => {
+  Twit.get(
+    'search/tweets',
+    { from: 'admkc', count: 10 },
+    (err, data, response) => {
+      if (err) {
+        return res.status(400).json({ message: 'Oops, bir hata oldu' })
+      }
+      res.status(200).json(JSON.stringify(data))
+    }
+  )
+}

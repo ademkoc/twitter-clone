07f3a451bd8364cbd2c9604ea5992c40fb4b168f
admkc <koc.adem.809@gmail.com>
2021-01-01 20:26:54 +0300

    loading indicator component

--- pages/index.js
+++ pages/index.js
@@ -1,8 +1,11 @@
 import React from 'react'
 import useSWR from 'swr'
 
+import styles from './index.module.css'
+
 import Layout from '../components/layout'
 import Tweet from '../components/tweet'
+import Loading from '../components/loading'
 import fetcher from '../lib/fetch'
 
 export default function HomePage() {
@@ -10,7 +13,11 @@ export default function HomePage() {
 
   return (
     <Layout>
-      {!data && <p>Loading</p>}
+      {!data && (
+        <div className={styles.loading}>
+          <Loading />
+        </div>
+      )}
 
       {data?.statuses?.map((twett) => (
         <Tweet key={twett.id} {...twett} />
